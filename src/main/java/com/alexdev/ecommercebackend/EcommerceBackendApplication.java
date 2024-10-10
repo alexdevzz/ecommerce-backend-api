153e681df3c16349d93abc7b56b44a5ea73ac435
alexdev <alexdev.workenv@gmail.com>
2024-10-10 00:42:46 -0400

    Create the basic structure for the proyect with devtools, jpa, mysql-connector,thymeleaf and starter-web

---
+++ src/main/java/com/alexdev/ecommercebackend/EcommerceBackendApplication.java
@@ -0,0 +1,14 @@
+package com.alexdev.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
+
+}
